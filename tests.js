b6b62cd4aac25f6f6e097de346cc4e1a19663e97
Sam Meech-Ward <sam@meech-ward.com>
2019-09-26 16:28:09 -0700

    Make the thest output more descriptive

--- tests.js
+++ tests.js
@@ -1,19 +1,31 @@
 const tests = {
   'calculate-values': () => {
     describe('calculateValues', function () {
-      it('should correctly calculate all the values in the array', function () {
+      it('should return 1 when given ["1"]', function () {
         chai.expect(testFunction(["1"])).to.equal(1);
+      });
+      it('should return 1 when given ["1", "+", "0"]', function () {
         chai.expect(testFunction(["1", "+", "0"])).to.equal(1);
+      });
+      it('should return 2 when given ["1", "+", "1"]', function () {
         chai.expect(testFunction(["1", "+", "1"])).to.equal(2);
+      });
+      it('should return 0 when given ["1", "-", "1"]', function () {
         chai.expect(testFunction(["1", "-", "1"])).to.equal(0);
       });
     });
   },
   'count-letters': () => {
-    it('should correctly count the number of letters in the string', function () {
+    it('should return {} when given ""', function () {
       chai.assert.deepEqual(testFunction(""), {});
+    });
+    it('should return {"a": 1} when given "a"', function () {
       chai.assert.deepEqual(testFunction("a"), {"a": 1});
+    });
+    it('should return {"a": 2} when given "aa"', function () {
       chai.assert.deepEqual(testFunction("aa"), {"a": 2});
+    });
+    it('should return {"a": 1, "b": 1} when given "ab"', function () {
       chai.assert.deepEqual(testFunction("ab"), {"a": 1, "b": 1});
     });
   }
