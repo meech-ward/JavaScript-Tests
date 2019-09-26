df91b9f6f967b7d38798be8e318d8fb152bc32ea
Sam Meech-Ward <sam@meech-ward.com>
2019-09-26 16:21:16 -0700

    Remove link when submitting tests

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function runTest(func, code) {
 const $functionSelect = $("#function-select");
 
 $("#test-form").on("submit", function(event) {
-  $(this).remove();
+  $('.test-form-container').remove();
   event.preventDefault();
   mocha.setup('bdd');
   runTest($functionSelect.val(), editor.getValue());
